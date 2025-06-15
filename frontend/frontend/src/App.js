e12c1786ddd0a805622620302ef908cd1ebe9a16
Angel Robles-Ortiz <angelroblesortiz.26@gmail.com>
2025-06-15 02:44:05 -0400

    added pokemon display

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -19,7 +19,51 @@ function App() {
   }, []);
 
   return (
-    <h1>{pokemon ? pokemon.name : "Loading..."}</h1>
+    <div>
+      <header className = 'pokedex-header'>
+        <img
+          src = {process.env.PUBLIC_URL + '/images/pokemonLogo.png'}
+          alt = 'Pokédex Logo'
+          className = 'pokedex-logo'
+        />
+      </header>
+      <div className = 'container'>
+        <div className = 'pokemon-card'>
+          <div className = 'pkmn-name-id-img'>
+            <div className = 'pkmn-name-id'>
+              <h1>{pokemon ? pokemon.name : 'Loading...'}</h1>
+              <h2>#{pokemon ? pokemon.id.toString().padStart(4, '0') : '0000'}</h2>
+            </div>
+
+            <div className = 'pkmn-img'>
+              <img src = {pokemon ? pokemon.img : process.env.PUBLIC_URL + '/images/pokeball.png'} 
+              alt = 'Pokemon' 
+              className = 'pokemon-image' />
+            </div>
+          </div>
+
+          <div className = 'pkmn-info'>
+            <div className = 'pkmn-type-region'>
+              <div className = 'pkmn-type'>
+                <p className = 'pkmn-type-text'>Type</p>
+                <div className = 'pkmn-type-img-container'>
+                  blank
+                </div>
+              </div>
+
+              <div className = 'pkmn-region'>
+                <p className = 'pkmn-region-text'>Region</p>
+                <div className = 'pkmn-region-container'>
+                  blank
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   );
 }
 
